d2b3c0f690736fad4896bdffb1d08356b9159779
Vasili Pl <w2e3r4t51982@gmail.com>
2025-11-02 20:24:42 +0100

    Версия 1.0

--- src/test/java/config/UserConfig.java
+++ src/test/java/config/UserConfig.java
@@ -9,10 +9,8 @@ import org.aeonbits.owner.Config;
 public interface UserConfig extends Config {
 
     @Key("userName")
-    @DefaultValue("basil8") // дефолтное значение, если в properties не указано
     String getUserName();
 
     @Key("password")
-    @DefaultValue("Basil1982!") // дефолтное значение
     String getPassword();
 }
